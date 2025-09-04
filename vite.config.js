d77212a417ca0cc8a2bfbccfeebbb77a81760c14
royemosby <roy.e.mosby@gmail.com>
2025-09-04 13:11:19 -0400

    alias mighty folders

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.csv'],
+  resolve: {
+    alias: {
+      '@services': path.resolve(__dirname, 'src/services'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@features': path.resolve(__dirname, 'src/features'),
+      '@routes': path.resolve(__dirname, 'src/routes'),
+    },
+  },
 });
