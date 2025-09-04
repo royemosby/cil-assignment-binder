02f86c317cb5a34c5b0b52c41899b2798838f76f
royemosby <roy.e.mosby@gmail.com>
2025-09-04 13:13:00 -0400

    change assignments route to upload

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,14 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import Admin from '../features/admin/Admin';
-import Assignments from '../features/assignments/Assignments';
 import Index from '../features/Index';
-
+import Upload from '../features/upload/Upload';
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Index />} />
       <Route path="/admin" element={<Admin />} />
-      <Route path="/assignments" element={<Assignments />} />
+      <Route path="/upload" element={<Upload />} />
     </Routes>
   );
 }
