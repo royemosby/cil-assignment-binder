d9186d31e79bfda8ce1c18853aa98ea1c3843b55
royemosby <roy.e.mosby@gmail.com>
2025-09-08 11:58:23 -0400

    rename Index to Binder

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,11 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
 import Admin from '../features/admin/Admin';
-import Index from '../features/Index';
+import Binder from '../features/binder/Binder';
 import Upload from '../features/upload/Upload';
 export default function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Index />} />
+      <Route path="/" element={<Binder />} />
       <Route path="/admin" element={<Admin />} />
       <Route path="/upload" element={<Upload />} />
     </Routes>
