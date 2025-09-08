20b7de5ce904d0e2aed43f6872a797fca8d9f8b1
royemosby <roy.e.mosby@gmail.com>
2025-09-08 11:58:09 -0400

    auto load csv for dx

--- src/features/upload/UploadForm.jsx
+++ src/features/upload/UploadForm.jsx
@@ -1,16 +1,27 @@
 import { processAssignmentsCSV } from '@services/assignmentsParser';
+import { useEffect } from 'react';
+import testData from '../../TestAssignments.csv';
 
-export default function UploadForm({
-  setWorkingAssignments,
-  setWorkingFields,
-}) {
+export default function UploadForm({ setAssignments, setFields }) {
+  //TODO delete after
+  useEffect(() => {
+    const loadTestData = async () => {
+      const response = await fetch(testData);
+      const text = await response.text();
+      const parsed = await processAssignmentsCSV(text);
+      setFields(parsed.meta.fields);
+      setAssignments(parsed.data);
+    };
+    loadTestData();
+  }, []);
+
   const handleCsvUpload = async (e) => {
     const file = e.target.files[0];
     if (file) {
       const text = await file.text();
       const parsed = await processAssignmentsCSV(text);
-      setWorkingFields(parsed.meta.fields);
-      setWorkingAssignments(parsed.data);
+      setFields(parsed.meta.fields);
+      setAssignments(parsed.data);
     }
   };
 
