2237ebeed616e7c45e5be6546b7365aa71bee71e
royemosby <roy.e.mosby@gmail.com>
2025-09-10 14:36:33 -0400

    add theme provider, organize imports

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,20 @@
+import { Theme } from '@radix-ui/themes';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import '@radix-ui/themes/styles.css';
+import './assets/styles/css-reset.css';
 import './assets/styles/reasonable-colors.css';
 import './index.css';
-import './reset.css';
-import './assets/styles/css-reset.css';
 import App from './App.jsx';
 
 import { BrowserRouter } from 'react-router';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Theme accentColor="blue" grayColor="sage" radius="small">
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Theme>
   </StrictMode>
 );
