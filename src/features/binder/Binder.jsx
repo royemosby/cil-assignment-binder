3b58fcef2120f0729e7cadffa72da4b6aab890c5
royemosby <roy.e.mosby@gmail.com>
2025-09-10 14:37:27 -0400

    add empty message

--- src/features/binder/Binder.jsx
+++ src/features/binder/Binder.jsx
@@ -1,7 +1,14 @@
+import { Link } from 'react-router';
+
 export default function Binder() {
   return (
     <>
       <h1>Binder</h1>
+      <p>
+        Looks a bit empty here! Get started by{' '}
+        <Link to="/upload">uploading a CSV</Link> from the assignments view in
+        Airtable
+      </p>
     </>
   );
 }
