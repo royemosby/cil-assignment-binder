f206474c8b295c8639d782ddc17cd14d2c079ad5
royemosby <roy.e.mosby@gmail.com>
2025-09-10 10:32:14 -0400

    repurpose assignments table

---
+++ src/features/binder/Assignments.jsx
@@ -0,0 +1,22 @@
+export default function Assignments({ fields, assignments }) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          {fields.map((field) => (
+            <th key={field}>{field}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {assignments.map((assignment) => (
+          <tr key={assignment.localId}>
+            {fields.map((field) => (
+              <td key={field}>{assignment[field]}</td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
