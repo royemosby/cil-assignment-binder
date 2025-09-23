38294b44bb59e83710f060f824b96eea15479311
royemosby <roy.e.mosby@gmail.com>
2025-09-23 15:08:50 -0400

    rename file

---
+++ src/services/persistence/db.js
@@ -0,0 +1,11 @@
+import Dexie from 'dexie';
+
+//not in use yet!
+export const db = new Dexie('cil-assignment-binder');
+db.version(1).stores({
+  cohorts: '++id, name, startDate, endDate, config, details',
+
+  students: '++id, cohortId, name, currentStatus, slack, slackUrl, auditStatus',
+  submissions:
+    '++id, studentId, submittedOn, week, topic, url, reviewCompleted, assignmentStatus, reviewedCompletedBy, reviewedOn',
+});
