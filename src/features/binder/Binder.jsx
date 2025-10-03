41e735912d608c6666baeff239684f5cbdb37f1f
royemosby <roy.e.mosby@gmail.com>
2025-10-03 15:00:52 -0400

    refactor Binder component to display submissions in tabs; improve loading state handling

--- src/features/binder/Binder.jsx
+++ src/features/binder/Binder.jsx
@@ -1,14 +1,35 @@
+import styles from './Binder.module.css';
 import { Link } from 'react-router';
+import { Tabs } from 'radix-ui';
+import Assignments from './Assignments';
+import Students from './Students/Students';
+import { useSubmissions } from '@services/persistence/hooks';
 
 export default function Binder() {
+  const { submissions } = useSubmissions();
   return (
     <>
       <h1>Binder</h1>
-      <p>
-        Looks a bit empty here! Get started by{' '}
-        <Link to="/upload">uploading a CSV</Link> from the assignments view in
-        Airtable
-      </p>
+      {submissions ? (
+        <Tabs.Root defaultValue="assignments">
+          <Tabs.List className={styles.tabsList}>
+            <Tabs.Trigger value="assignments">Assignments</Tabs.Trigger>
+            <Tabs.Trigger value="students">Students</Tabs.Trigger>
+          </Tabs.List>
+          <Tabs.Content value="assignments">
+            <Assignments />
+          </Tabs.Content>
+          <Tabs.Content value="students">
+            <Students />
+          </Tabs.Content>
+        </Tabs.Root>
+      ) : (
+        <p>
+          Looks a bit empty here! Get started by{' '}
+          <Link to="/upload">uploading a CSV</Link> from the assignments view in
+          Airtable
+        </p>
+      )}
     </>
   );
 }
